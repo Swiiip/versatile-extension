d87810de16cefb4635952f36331e03f269f98402
Swiiip <hugo.duthil@gmail.com>
2017-02-01 03:14:45 +0100

    Introduce snippets in provideCompletionItems

--- src/extension.ts
+++ src/extension.ts
@@ -9,14 +9,12 @@ export function activate(context: vscode.ExtensionContext) {
     console.log("Versatile language support loaded")
     vscode.languages.registerCompletionItemProvider('versatile', {
         provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
-            let item: vscode.CompletionItem = new vscode.CompletionItem('select', vscode.CompletionItemKind.Function);
-            let item2: vscode.CompletionItem = new vscode.CompletionItem('?equal', vscode.CompletionItemKind.Function);
-            let item3: vscode.CompletionItem = new vscode.CompletionItem('if', vscode.CompletionItemKind.Function);
-            let item4: vscode.CompletionItem = new vscode.CompletionItem('ifelseif', vscode.CompletionItemKind.Function);
-            let item5: vscode.CompletionItem = new vscode.CompletionItem('proposal', vscode.CompletionItemKind.Field);
-            let item6: vscode.CompletionItem = new vscode.CompletionItem('slot', vscode.CompletionItemKind.Field);
+            let item: vscode.CompletionItem = new vscode.CompletionItem('if', vscode.CompletionItemKind.Function)
+            let range: vscode.Range = document.getWordRangeAtPosition(position)
+            item.detail = "Details on the select method"
+            item.textEdit = vscode.TextEdit.replace(range, 'if ({{element}}, {{ifExists}}, {{ifNot}})')
             return [
-                item, item2, item3, item4, item5, item6
+                item
             ];
         }
     });
