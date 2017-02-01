4cf08cbdf411a331ae05eb17cdcfc93a40706aa7
Hugo Duthil <h.duthil@criteo.com>
2017-02-01 18:49:37 +0100

    Add resources folder

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,28 @@
+import * as path from 'path';
 // VS Code extensibility API
 import * as vscode from 'vscode';
+import * as fs from 'fs' // file system
 
 export function activate(context: vscode.ExtensionContext) {
 
-    // The most simple completion item provider which 
+    // The most simple completion item provider which
     // * registers for text files (`'plaintext'`), and
     // * only return the 'Hello World' completion
     console.log("Versatile language support loaded")
-    vscode.languages.registerCompletionItemProvider('versatile', {
-        provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
-            let item: vscode.CompletionItem = new vscode.CompletionItem('if', vscode.CompletionItemKind.Function)
-            let range: vscode.Range = document.getWordRangeAtPosition(position)
-            item.detail = "Details on the select method"
-            item.textEdit = vscode.TextEdit.replace(range, 'if ({{element}}, {{ifExists}}, {{ifNot}})')
-            return [
-                item
-            ];
+    fs.readFile(path.join(__dirname, "../../resources/functions.json"), "utf8", (err, data) => {
+        let items: vscode.CompletionItem[] = [];
+        let functions: any = JSON.parse(data).functions;
+        for (let f of functions) {
+            let item: vscode.CompletionItem = new vscode.CompletionItem(f.name, vscode.CompletionItemKind.Function)
+            item.insertText = f.pattern
+            item.detail = f.detail
+            item.documentation = f.doc
+            items.push(item);
         }
-    });
+        vscode.languages.registerCompletionItemProvider('versatile', {
+            provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
+                return items;
+            }
+        });
+    })
 }
