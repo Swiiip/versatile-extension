8764d5a09f04c9d717db47dcbcb47c024e56a0ea
Swiiip <hugo.duthil@gmail.com>
2017-02-02 01:49:58 +0100

    Add dirty signature helper

--- src/extension.ts
+++ src/extension.ts
@@ -9,20 +9,80 @@ export function activate(context: vscode.ExtensionContext) {
     // * registers for text files (`'plaintext'`), and
     // * only return the 'Hello World' completion
     console.log("Versatile language support loaded")
-    fs.readFile(path.join(__dirname, "../../resources/functions.json"), "utf8", (err, data) => {
-        let items: vscode.CompletionItem[] = [];
-        let functions: any = JSON.parse(data).functions;
-        for (let f of functions) {
-            let item: vscode.CompletionItem = new vscode.CompletionItem(f.name, vscode.CompletionItemKind.Function)
-            item.insertText = f.pattern
-            item.detail = f.detail
-            item.documentation = f.doc
-            items.push(item);
-        }
+    fs.readFile(path.join(__dirname, "../../resources/completions.json"), "utf8", (err, data) => {
+        // Retrieve function descriptions
+        let functions: Object[] = JSON.parse(data).functions;
+
+        // Create a CompletionList from the descriptions
+        let functionCompletionList: vscode.CompletionList = buildFunctionCompletionItems(functions);
+
+        // Register the function completion item provider
         vscode.languages.registerCompletionItemProvider('versatile', {
             provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
-                return items;
+                return functionCompletionList;
             }
         });
+
+        // Register the function signature help provider
+        vscode.languages.registerSignatureHelpProvider('versatile', {
+            provideSignatureHelp(document: vscode.TextDocument, position: vscode.Position) {
+                let start = position.translate(0, -1)
+                let end = position
+                let text = document.getText(new vscode.Range(start, end));
+                while (!text.match(/(\w)\s?\([\w\s,]*/g)) {
+                    start = start.translate(0, -1)
+                    text = document.getText(new vscode.Range(start, end));
+                }
+                let commaMatch = text.match(/,/g)
+                let activeParam = commaMatch ? commaMatch.length : 0
+                let functionName = document.getText(document.getWordRangeAtPosition(start))
+                let f = functions[functionName]
+                if (f) {
+                    let signatureHelp = new vscode.SignatureHelp()
+                    let signatureDef = generateDefinitionString(functionName, f.parameters)
+                    let signatureInfo = new vscode.SignatureInformation(signatureDef, f.doc)
+                    for (let index in f.parameters) {
+                        let idx = Number(index)
+                        let param = f.parameters[idx]
+                        let signatureParam: string = ""
+                        signatureParam += param.name
+                        if (param.type)
+                            signatureParam += ": " + param.type
+                        signatureInfo.parameters.push(new vscode.ParameterInformation(signatureParam, param.description))
+                    }
+                    signatureHelp.signatures.push(signatureInfo)
+                    signatureHelp.activeParameter = activeParam
+                    signatureHelp.activeSignature = 0
+                    return signatureHelp;
+                } else {
+                    return null;
+                }
+            }
+        }, ",", "(");
     })
+}
+
+function buildFunctionCompletionItems(functions: Object): vscode.CompletionList {
+    let completionList: vscode.CompletionList = new vscode.CompletionList()
+    for (let name of Object.keys(functions)) {
+        let f = functions[name]
+        let item: vscode.CompletionItem = new vscode.CompletionItem(name, vscode.CompletionItemKind.Function)
+        item.insertText = f.pattern
+
+        item.detail = generateDefinitionString(name, f.parameters)
+            item.documentation = f.doc
+        completionList.items.push(item)
+    }
+    return completionList;
+}
+
+function generateDefinitionString(functionName: string, params: any[]) {
+    let concatenatedParams: string[] = []
+    for (let param of params) {
+        let p = param.name
+        if (param.type && param.type !== "")
+            p += ": " + param.type
+        concatenatedParams.push(p)
+    }
+    return functionName + "(" + concatenatedParams.join(", ") + ")"
 }
